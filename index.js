085c555dd11c517ca358e9d0136dc2b4b1c89952
bubinq <emilbumov@gmail.com>
2022-10-05 13:27:04 +0300

    added base functionality

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,9 +21,38 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+app.get('/api', (req, res) => {
+  const respObj = {}
+  const rawDate = new Date().valueOf()
+  respObj['unix'] = new Date(rawDate).getTime()
+  respObj['utc'] = new Date(rawDate).toUTCString()
+
+  res.json({ "unix": respObj.unix, "utc": respObj.utc })
+})
+
+app.get('/api/:date', (req, res) => {
+  const rawDate = req.params.date
+  const respObj = {}
+  if (Number(rawDate)) {
+    respObj['unix'] = new Date(Number(rawDate)).getTime()
+    respObj['utc'] = new Date(Number(rawDate)).toUTCString()
+      
+  } else {
+    respObj['unix'] = new Date(rawDate).getTime()
+    respObj['utc'] = new Date(rawDate).toUTCString()
+  }
+
+  if (!respObj.unix || !respObj.utc) {
+    res.json({ "error": "Invalid Date" })
+  } else {
+
+    res.json({ "unix": respObj.unix, "utc": respObj.utc })
+  }
+    
+})
 
 
 // listen for requests :)
